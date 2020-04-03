634805a8b5a9fa28d3085ceafb7d543e4e028543
ffadiyah <ffadiyah@gmail.com>
2020-04-03 20:14:53 +0700

    save

---
+++ RESTApi/RESTApi/Data/RESTApiContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace RESTApi.Models
+{
+    public class RESTApiContext : DbContext
+    {
+        public RESTApiContext (DbContextOptions<RESTApiContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<RESTApi.Models.Movie> Movie { get; set; }
+    }
+}
--- RESTApi/RESTApi/Startup.cs
+++ RESTApi/RESTApi/Startup.cs
@@ -10,6 +10,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.EntityFrameworkCore;
+using RESTApi.Models;
 
 namespace RESTApi
 {
@@ -26,6 +28,9 @@ namespace RESTApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<RESTApiContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("RESTApiContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
